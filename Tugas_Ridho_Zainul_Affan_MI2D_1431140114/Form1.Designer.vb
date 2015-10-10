ec7dc72c5f4ea6ad5207ddb8229e76b66a8247e4
ridhoaffan96 <ridhoaffan96@gmail.com>
2015-10-10 15:10:36 +0700

    TUGAS KALKULATOR

---
+++ Tugas_Ridho_Zainul_Affan_MI2D_1431140114/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.tombol7 = New System.Windows.Forms.Button()
+        Me.tombol8 = New System.Windows.Forms.Button()
+        Me.tombol9 = New System.Windows.Forms.Button()
+        Me.tombol4 = New System.Windows.Forms.Button()
+        Me.tombol5 = New System.Windows.Forms.Button()
+        Me.tombol6 = New System.Windows.Forms.Button()
+        Me.tombol1 = New System.Windows.Forms.Button()
+        Me.tombol2 = New System.Windows.Forms.Button()
+        Me.tombol3 = New System.Windows.Forms.Button()
+        Me.tombol0 = New System.Windows.Forms.Button()
+        Me.tombolSamaDengan = New System.Windows.Forms.Button()
+        Me.tombolClear = New System.Windows.Forms.Button()
+        Me.tombolKurang = New System.Windows.Forms.Button()
+        Me.tombolTambah = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox1.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "0"
+        '
+        'tombol7
+        '
+        Me.tombol7.Location = New System.Drawing.Point(12, 38)
+        Me.tombol7.Name = "tombol7"
+        Me.tombol7.Size = New System.Drawing.Size(57, 52)
+        Me.tombol7.TabIndex = 1
+        Me.tombol7.Text = "7"
+        Me.tombol7.UseVisualStyleBackColor = True
+        '
+        'tombol8
+        '
+        Me.tombol8.Location = New System.Drawing.Point(75, 38)
+        Me.tombol8.Name = "tombol8"
+        Me.tombol8.Size = New System.Drawing.Size(62, 52)
+        Me.tombol8.TabIndex = 2
+        Me.tombol8.Text = "8"
+        Me.tombol8.UseVisualStyleBackColor = True
+        '
+        'tombol9
+        '
+        Me.tombol9.Location = New System.Drawing.Point(143, 38)
+        Me.tombol9.Name = "tombol9"
+        Me.tombol9.Size = New System.Drawing.Size(57, 52)
+        Me.tombol9.TabIndex = 3
+        Me.tombol9.Text = "9"
+        Me.tombol9.UseVisualStyleBackColor = True
+        '
+        'tombol4
+        '
+        Me.tombol4.Location = New System.Drawing.Point(12, 96)
+        Me.tombol4.Name = "tombol4"
+        Me.tombol4.Size = New System.Drawing.Size(57, 50)
+        Me.tombol4.TabIndex = 4
+        Me.tombol4.Text = "4"
+        Me.tombol4.UseVisualStyleBackColor = True
+        '
+        'tombol5
+        '
+        Me.tombol5.Location = New System.Drawing.Point(75, 96)
+        Me.tombol5.Name = "tombol5"
+        Me.tombol5.Size = New System.Drawing.Size(62, 50)
+        Me.tombol5.TabIndex = 5
+        Me.tombol5.Text = "5"
+        Me.tombol5.UseVisualStyleBackColor = True
+        '
+        'tombol6
+        '
+        Me.tombol6.Location = New System.Drawing.Point(143, 96)
+        Me.tombol6.Name = "tombol6"
+        Me.tombol6.Size = New System.Drawing.Size(57, 50)
+        Me.tombol6.TabIndex = 6
+        Me.tombol6.Text = "6"
+        Me.tombol6.UseVisualStyleBackColor = True
+        '
+        'tombol1
+        '
+        Me.tombol1.Location = New System.Drawing.Point(12, 152)
+        Me.tombol1.Name = "tombol1"
+        Me.tombol1.Size = New System.Drawing.Size(57, 49)
+        Me.tombol1.TabIndex = 7
+        Me.tombol1.Text = "1"
+        Me.tombol1.UseVisualStyleBackColor = True
+        '
+        'tombol2
+        '
+        Me.tombol2.Location = New System.Drawing.Point(75, 152)
+        Me.tombol2.Name = "tombol2"
+        Me.tombol2.Size = New System.Drawing.Size(62, 49)
+        Me.tombol2.TabIndex = 8
+        Me.tombol2.Text = "2"
+        Me.tombol2.UseVisualStyleBackColor = True
+        '
+        'tombol3
+        '
+        Me.tombol3.Location = New System.Drawing.Point(143, 152)
+        Me.tombol3.Name = "tombol3"
+        Me.tombol3.Size = New System.Drawing.Size(57, 49)
+        Me.tombol3.TabIndex = 9
+        Me.tombol3.Text = "3"
+        Me.tombol3.UseVisualStyleBackColor = True
+        '
+        'tombol0
+        '
+        Me.tombol0.Location = New System.Drawing.Point(12, 207)
+        Me.tombol0.Name = "tombol0"
+        Me.tombol0.Size = New System.Drawing.Size(57, 55)
+        Me.tombol0.TabIndex = 10
+        Me.tombol0.Text = "0"
+        Me.tombol0.UseVisualStyleBackColor = True
+        '
+        'tombolSamaDengan
+        '
+        Me.tombolSamaDengan.Location = New System.Drawing.Point(75, 207)
+        Me.tombolSamaDengan.Name = "tombolSamaDengan"
+        Me.tombolSamaDengan.Size = New System.Drawing.Size(125, 55)
+        Me.tombolSamaDengan.TabIndex = 11
+        Me.tombolSamaDengan.Text = "="
+        Me.tombolSamaDengan.UseVisualStyleBackColor = True
+        '
+        'tombolClear
+        '
+        Me.tombolClear.Location = New System.Drawing.Point(206, 38)
+        Me.tombolClear.Name = "tombolClear"
+        Me.tombolClear.Size = New System.Drawing.Size(66, 108)
+        Me.tombolClear.TabIndex = 13
+        Me.tombolClear.Text = "Clear"
+        Me.tombolClear.UseVisualStyleBackColor = True
+        '
+        'tombolKurang
+        '
+        Me.tombolKurang.Location = New System.Drawing.Point(206, 152)
+        Me.tombolKurang.Name = "tombolKurang"
+        Me.tombolKurang.Size = New System.Drawing.Size(66, 49)
+        Me.tombolKurang.TabIndex = 14
+        Me.tombolKurang.Text = "-"
+        Me.tombolKurang.UseVisualStyleBackColor = True
+        '
+        'tombolTambah
+        '
+        Me.tombolTambah.Location = New System.Drawing.Point(206, 207)
+        Me.tombolTambah.Name = "tombolTambah"
+        Me.tombolTambah.Size = New System.Drawing.Size(66, 55)
+        Me.tombolTambah.TabIndex = 15
+        Me.tombolTambah.Text = "+"
+        Me.tombolTambah.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 281)
+        Me.Controls.Add(Me.tombolTambah)
+        Me.Controls.Add(Me.tombolKurang)
+        Me.Controls.Add(Me.tombolClear)
+        Me.Controls.Add(Me.tombolSamaDengan)
+        Me.Controls.Add(Me.tombol0)
+        Me.Controls.Add(Me.tombol3)
+        Me.Controls.Add(Me.tombol2)
+        Me.Controls.Add(Me.tombol1)
+        Me.Controls.Add(Me.tombol6)
+        Me.Controls.Add(Me.tombol5)
+        Me.Controls.Add(Me.tombol4)
+        Me.Controls.Add(Me.tombol9)
+        Me.Controls.Add(Me.tombol8)
+        Me.Controls.Add(Me.tombol7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents tombol7 As System.Windows.Forms.Button
+    Friend WithEvents tombol8 As System.Windows.Forms.Button
+    Friend WithEvents tombol9 As System.Windows.Forms.Button
+    Friend WithEvents tombol4 As System.Windows.Forms.Button
+    Friend WithEvents tombol5 As System.Windows.Forms.Button
+    Friend WithEvents tombol6 As System.Windows.Forms.Button
+    Friend WithEvents tombol1 As System.Windows.Forms.Button
+    Friend WithEvents tombol2 As System.Windows.Forms.Button
+    Friend WithEvents tombol3 As System.Windows.Forms.Button
+    Friend WithEvents tombol0 As System.Windows.Forms.Button
+    Friend WithEvents tombolSamaDengan As System.Windows.Forms.Button
+    Friend WithEvents tombolClear As System.Windows.Forms.Button
+    Friend WithEvents tombolKurang As System.Windows.Forms.Button
+    Friend WithEvents tombolTambah As System.Windows.Forms.Button
+
+End Class
---
+++ Tugas_Ridho_Zainul_Affan_MI2D_1431140114/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Dim input_angka, Hasil As Integer
+
+    Dim Rumus As String
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles tombol6.Click
+        TextBox1.Text = TextBox1.Text & "6"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles tombol2.Click
+        TextBox1.Text = TextBox1.Text & "2"
+    End Sub
+
+    Private Sub tombol1_Click(sender As Object, e As EventArgs) Handles tombol1.Click
+        TextBox1.Text = TextBox1.Text & "1"
+    End Sub
+
+    Private Sub tombolSamaDengan_Click(sender As Object, e As EventArgs) Handles tombolSamaDengan.Click
+        Select Case Rumus
+            Case "+"
+                Hasil = input_angka + TextBox1.Text
+            Case "-"
+                Hasil = input_angka - TextBox1.Text
+        End Select
+        TextBox1.Text = Hasil
+    End Sub
+
+    Private Sub tombolKurang_Click(sender As Object, e As EventArgs) Handles tombolKurang.Click
+        input_angka = TextBox1.Text
+        Rumus = "-"
+        TextBox1.Text = Nothing
+    End Sub
+
+    Private Sub tombolTambah_Click(sender As Object, e As EventArgs) Handles tombolTambah.Click
+        input_angka = TextBox1.Text
+        Rumus = "+"
+        TextBox1.Text = Nothing
+    End Sub
+
+    Private Sub tombolClear_Click(sender As Object, e As EventArgs) Handles tombolClear.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub tombol0_Click(sender As Object, e As EventArgs) Handles tombol0.Click
+        TextBox1.Text = TextBox1.Text & "0"
+    End Sub
+
+    Private Sub tombol3_Click(sender As Object, e As EventArgs) Handles tombol3.Click
+        TextBox1.Text = TextBox1.Text & "3"
+    End Sub
+
+    Private Sub tombol4_Click(sender As Object, e As EventArgs) Handles tombol4.Click
+        TextBox1.Text = TextBox1.Text & "4"
+    End Sub
+
+    Private Sub tombol5_Click(sender As Object, e As EventArgs) Handles tombol5.Click
+        TextBox1.Text = TextBox1.Text & "5"
+    End Sub
+
+    Private Sub tombol7_Click(sender As Object, e As EventArgs) Handles tombol7.Click
+        TextBox1.Text = TextBox1.Text & "7"
+    End Sub
+
+    Private Sub tombol8_Click(sender As Object, e As EventArgs) Handles tombol8.Click
+        TextBox1.Text = TextBox1.Text & "8"
+    End Sub
+
+    Private Sub tombol9_Click(sender As Object, e As EventArgs) Handles tombol9.Click
+        TextBox1.Text = TextBox1.Text & "9"
+    End Sub
+End Class
